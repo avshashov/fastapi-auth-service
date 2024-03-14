9a09b2b8e9633af830c5a0c9493b298a9b4921ce
Artem Shashov <tauquantum@gmail.com>
2024-03-14 19:47:09 +1000

    refactor: dosc, return values

--- app/auth/middleware.py
+++ app/auth/middleware.py
@@ -1,3 +1,6 @@
+"""
+Authentication middleware.
+"""
 from fastapi import Request
 from starlette.authentication import AuthCredentials, AuthenticationBackend
 
@@ -9,12 +12,12 @@ from app.schemas import UserWithMetaOutput
 class AuthBackend(AuthenticationBackend):
     """
     This is a custom auth backend class that will allow you to authenticate
-    your request and return auth and user as a tuple
+    your request and return auth and user as a tuple.
     """
 
-    async def authenticate(self, request: Request) -> tuple[AuthCredentials, UserWithMetaOutput] | None:
+    async def authenticate(self, request: Request) -> tuple[AuthCredentials, UserWithMetaOutput | None]:
         # запросы '/auth/login', '/user/signup' должны работать без токена
         if request.url.path in PathsWithoutAuthHeaders:
-            return
+            return AuthCredentials(['unauthenticated']), None
         user = await AuthService.validate_access_token_and_get_user(request)
         return AuthCredentials(['authenticated']), user
